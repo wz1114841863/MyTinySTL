2997b45a58fdf368f3631d57943c594831cd34aa
wz1114841863 <18801383735@163.com>
2023-02-24 09:42:02 +0800

    添加algorithm文件，cmake文件架构不合理

---
+++ MyTinySTL/algorithm/algo.h
@@ -0,0 +1,4 @@
+#ifndef TINYSTL_ALGO_H
+#define TINYSTL_ALGO_H
+
+#endif //TINYSTL_ALGO_H
---
+++ MyTinySTL/algorithm/numeric.h
@@ -0,0 +1,130 @@
+#ifndef TINYSTL_NUMERIC_H
+#define TINYSTL_NUMERIC_H
+
+// 数值算法
+
+#include "iterator.h"
+namespace mystl {
+
+    /*****************************************************************************************/
+    // accumulate
+    // 版本1：以初值 init 对每个元素进行累加
+    // 版本2：以初值 init 对每个元素进行二元操作
+    /*****************************************************************************************/
+    template <typename InputIter, typename T>
+    T accumulate(InputIter first, InputIter last, T init) {
+        for (; first != last; ++first) {
+            inin += *first;
+        }
+
+        return init;
+    }
+
+    template <typename InputIter, typename T, typename BinaryOp>
+    T accumulate(InputIter first, InputIter last, T init, BinaryOp binary_op) {
+        for (; first != last; ++first) {
+            init = binary_op(init, *first);
+        }
+
+        return init;
+    }
+
+    /*****************************************************************************************/
+    // adjacent_difference
+    // 版本1：计算相邻元素的差值，结果保存到以 result 为起始的区间上
+    // 版本2：自定义相邻元素的二元操作
+    /*****************************************************************************************/
+    template <typename InputIter, typename OutputIter>
+    OutputIter adjacent_difference(InputIter first, InputIter last, OutputIter result) {
+        if (first == last) return result;
+        *result = *first;
+        auto value = *first;
+        while (++first != last) {
+            auto tmp = *first;
+            *++result = tmp - value;
+            value = tmp;
+        }
+        return ++result;
+    }
+
+    template <typename InputIter, typename OutputIter, typename BinaryOp>
+    OutputIter adjacent_difference(InputIter first, InputIter last, OutputIter result,
+                                   BinaryOp binary_op) {
+        if (first == last) return result;
+        *result = *first;
+        auto value = *first;
+        while (++first != last) {
+            auto tmp = *first;
+            *++result = binary_op(tmp, value);
+            value = tmp;
+        }
+        return ++result;
+    }
+
+    /*****************************************************************************************/
+    // inner_product
+    // 版本1：以 init 为初值，计算两个区间的内积
+    // 版本2：自定义 operator+ 和 operator*
+    /*****************************************************************************************/
+    template <typename InputIter1, typename InputIter2, typename T>
+    T inner_product(InputIter1 first1, InputIter1 last1, InputIter1 first2, T init) {
+        for (; first1 != last1; ++first1, ++first2) {
+            init += (*first1 * *first2);
+        }
+        return init;
+    }
+
+    template <typename InputIter1, typename InputIter2, typename T>
+    T inner_product(InputIter1 first1, InputIter1 last1, InputIter1 first2, T init,
+                    BinaryOp1 binary_op1, BinaryOp2 binary_op2) {
+        for (; first1 != last1; ++first1, ++first2) {
+            init = binary_op1(init, binary_op2(*first1, *first2));
+        }
+        return init;
+    }
+
+    /*****************************************************************************************/
+    // iota
+    // 填充[first, last)，以 value 为初值开始递增
+    /*****************************************************************************************/
+    template <typename ForwardIter, typename T>
+    void itoa(ForwardIter first, ForwardIter last, T value) {
+        while (first != last) {
+            *first++ = value;
+            ++value;
+        }
+    }
+
+    /*****************************************************************************************/
+    // partial_sum
+    // 版本1：计算局部累计求和，结果保存到以 result 为起始的区间上
+    // 版本2：进行局部进行自定义二元操作
+    /*****************************************************************************************/
+    template <typename InputIter, typename OutputIter>
+    OutputIter partial_sum(InputIter first, InputIter last, OutputIter result) {
+        if (first != last) return result;
+        *result = *first;  // 记录第一个元素
+        auto value = *first;
+        while (++first != last) {
+            value = value + *first;
+            *++result = value;
+        }
+        return ++result;
+    }
+
+    template <typename InputIter, typename OutputIter, typename BinaryOp>
+    OutputIter partial_sum(InputIter first, InputIter last, OutputIter result,
+                           BinaryOp binary_op) {
+        if (first == last) return result;
+        *result = *first;
+        auto value = *first;
+        while (++first != last) {
+            value = binary_op(value, *first);
+            *++result = value;
+        }
+
+        return ++result;
+    }
+
+}  // namespace mystl;
+#endif //TINYSTL_NUMERIC_H
